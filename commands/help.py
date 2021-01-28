097b4fb2db7d781cbe3e0b3978ed521ee2b17809
Jurzzyy <Ebun12@outlook.com>
2021-01-29 00:38:48 +0200

    with explanation this time

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,5 @@
 async def execute(msg, args):
-    await msg.channel.send("hello - esittelee Tonankan \n"
+    await msg.channel.send("hello - esittelee Tonankan \n" # creates a list with each command on their own line
         "help - listaa komennot ja niiden selitykset \n"
         "ping - näyttää botin pingin \n"
         "rng - antaa satunnaisen numeron kahden annetun, tai ennalta määritetyn " + \
