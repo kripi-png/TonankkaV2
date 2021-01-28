52476bf8fb0b2692c8d61a1623e59a3560438a8a
Jurzzyy <Ebun12@outlook.com>
2021-01-29 00:35:55 +0200

    kekw

---
+++ commands/help.py
@@ -0,0 +1,11 @@
+async def execute(msg, args):
+    await msg.channel.send("hello - esittelee Tonankan \n"
+        "help - listaa komennot ja niiden selitykset \n"
+        "ping - näyttää botin pingin \n"
+        "rng - antaa satunnaisen numeron kahden annetun, tai ennalta määritetyn " + \
+        "numeron väliltä")
+
+commandData = {
+    "name": "help",
+    "execute": lambda msg,arguments,*args : execute(msg, arguments) # katso ping.py:stä selitys *args-argumentille
+}
