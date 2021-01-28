fd4baca101c12d5160eca61c9b2c47df436584ab
Jurzzyy <Ebun12@outlook.com>
2021-01-28 23:11:37 +0200

    More fixes to rng.py

--- commands/rng.py
+++ commands/rng.py
@@ -5,15 +5,21 @@ async def execute(msg, args):
     if len(args) == 1: # if you enter no value, command will randomize between 0 and 100
         value = randint(0, 100)
 
-    elif len(args) == 2 and args[1].isnumeric(): # if only one number is given and is a numeral,
-        value = randint(0, int(args[1])) # will randomize between 0 and given number.
+    elif len(args) == 2 and args[1].lstrip("-").isnumeric(): # if only one number is given and is a numeral,
+        if int(args[1]) < 0: # if number is <0, command will randomize between given number and 0
+            value = randint(int(args[1]), 0)
+        else:
+            value = randint(0, int(args[1])) # will randomize between 0 and given number.
 
-    elif not args[1].isnumeric() or not args[2].isnumeric(): # if input is not a numeral, will output message
+    elif not args[1].lstrip("-").isnumeric() or not args[2].isnumeric(): # if input is not a numeral, will output message
         await msg.channel.send("Can't randomize numbers from words.")
         return
 
     else:
-        value = randint(int(args[1]), int(args[2])) # the 2 numbers after command defines the range of number generator
+        if args[2] < args[1]: # checks if second number is smaller than the first one
+            value = randint(int(args[2]), int(args[1]))
+        else:
+            value = randint(int(args[1]), int(args[2])) # the 2 numbers after command defines the range of number generator
 
     await msg.channel.send("Generated: " + str(value))
 
