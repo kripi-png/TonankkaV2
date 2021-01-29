265b46ddc534acc91978b0f1e9ce12d8ef1e3ef2
kripi__ <mainkriiper@gmail.com>
2021-01-29 21:34:54 +0200

    moved variable declarations inside the function

--- commandHandler.py
+++ commandHandler.py
@@ -1,10 +1,11 @@
 import os, importlib
 
-commandList = [x[0:-3] for x in os.listdir('commands') if x.endswith(".py") and not x.startswith('_')] # luo lista kaikkien komentojen nimistä
-commands = {}
-
 def loadCommands():
     print("Loading Commands...")
+    # luo lista kaikkien komentojen nimistä.
+    # x on komennon tiedoston nimi (ie. ping.py), joten x[0:-3] poistaa file-extensionin (.py)
+    commandList = [x[0:-3] for x in os.listdir('commands') if x.endswith(".py") and not x.startswith('_')]
+    commands = {}
     for com in commandList:
         commandData = importlib.import_module('commands.{0}'.format(com)).commandData
         commands[commandData['name']] = {
