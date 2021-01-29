aa19259d0f64bb4124b1b23585e5f82d73e41d78
kripi__ <mainkriiper@gmail.com>
2021-01-29 17:53:04 +0200

    createEmbed-funktio helpottamaan embed-viestien luomista
    defaul embed-viestin reunaväri

--- settings.py
+++ settings.py
@@ -1,1 +1,2 @@
 commandPrefix = '!'
+defaultEmbedColor = 0x00ff00
---
+++ utils.py
@@ -0,0 +1,22 @@
+from settings import defaultEmbedColor
+import discord
+
+# returns an embed message obje ct which can then be sent await msg.channel.send(embed=<embedMuuttuja>)
+def createEmbed(title="Title", desc="Desc", color=defaultEmbedColor, thumbnail=None, image=None, fields=None):
+    embed = discord.Embed(title=title, desc=desc, color=color)
+    if thumbnail:
+        embed.set_thumbnail(url=thumbnail)
+
+    if image:
+        embed.set_image(url=image)
+
+    if type(fields) is list:
+        for field in fields:
+            if not 'inline' in field.keys():
+                field['inline'] = False
+
+            embed.add_field(name=field["name"], value=field["value"], inline=field["inline"])
+    elif fields:
+        raise(Exception("An Error occured when creating an embed message - {}.\nError: Fields-argument has to be a list of dictionaries, even if there's only one.".format(title)))
+
+    return embed
