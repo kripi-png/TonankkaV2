2b768b0b2acb1793319400369cf4d6b564fe789a
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-09-24 09:17:31 +0300

    added comments and a debug channel

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 commandPrefix = '!'
 defaultEmbedColor = 0x00ff00
 engineerColor = 0x4e248d
-generalChannelID = 747713984181960767
-notificationChannelID = 825039268929994803
+
+generalChannelID = 747713984181960767 # general
+notificationChannelID = 825039268929994803 # tonankka-chan-nel
+debugChannelID = 775141725508337684 # a channel on kripi's private server
