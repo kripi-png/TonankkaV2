e799e873476dbee175aa831c4d48577edaddbca2
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-05-13 18:02:46 +0300

    function for priting detailed exception message

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-from settings import defaultEmbedColor
 import discord
+import sys
+
+from settings import defaultEmbedColor
 
 # returns an embed message obje ct which can then be sent await msg.channel.send(embed=<embedMuuttuja>)
 def createEmbed(title=None, desc=None, color=defaultEmbedColor, thumbnail=None, image=None, fields=None):
@@ -23,3 +25,13 @@ def createEmbed(title=None, desc=None, color=defaultEmbedColor, thumbnail=None, 
         raise(Exception("An Error occured when creating an embed message - {}.\nError: Fields-argument has to be a list of dictionaries, even if there's only one.".format(title)))
 
     return embed
+
+def detailed_exc_msg(e: Exception) -> None:
+    """Prints exception type, file, and line number."""
+    exception_traceback = sys.exc_info()[2]
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+
+    print(f"{e=}, {type(e)=}")
+    print("File name: ", filename)
+    print("Line number: ", line_number)
